e579e10855013bfa72bb84477612f369a120e690
laurenAMills <laurenmills@laurens-mbp.attlocal.net>
2014-07-16 21:19:59 -0700

    second push to repo.

---
+++ mills_lauren_assignment2.js
@@ -0,0 +1,20 @@
+// alert("JavaScript works!"); 
+// Lauren Mills
+// Project 2
+// SDI 1407
+//My very needed Vacation
+
+// Variables
+var daysTillVaca = 122;
+var locations = ("Italy", "Paris", "Germany", "Sevilla");
+
+// Procedure
+
+// Boolean Function
+
+// Number Function
+
+// String Function
+
+// Array Function
+
