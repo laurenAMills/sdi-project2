debd078e8acb8200b900427bf488df9a4a2f374d
laurenAMills <laurenmills@Laurens-MacBook-Pro.local>
2014-07-17 16:17:48 -0700

    third push.  re did parts of number, boolean and procedure.  Deleted array.

--- mills_lauren_assignment2.js
+++ mills_lauren_assignment2.js
@@ -16,50 +16,42 @@ var locations = [
 				"China"];
 var country = "Asia";
 var goingWith = "mom";
-var hrOfTraveling = 46;
+var hrOfTraveling = 44;
 var numOfNights = 14;
 
+
 // Procedure
-var firstStop = function (anyStop) {
-	anyStop = country;
-	if (country === "Asia"){
-	console.log("I cannot believe my " + goingWith + " and I are back in " + country + " for the first time 16 years!");  
-/*	
+var firstStop = function (anyStop) { 
+	if (anyStop == "Asia"){
+	console.log("I cannot believe my " + goingWith + " and I are back in " + country + " for the first time 16 years!");  	
 	} else {	
-		console.log("If I cannot go to " + country + " with my" + going With + " then im going to fly to Connecticut to see her.");
-		};
+		console.log("If I cannot go to " + country + " with my" + goingWith + " then im going to fly to Connecticut to see her.");
+	};
 };
-*/
 	
 // Boolean Function
-var whatToDo = function (spendingPerDay, numOfStops) {
-	if (spendingPerDay <=  250 && numOfStops >= 30) {
-	moreToBeDone = true;
+var whatToDo = function (spending, stops) {
+	if (spending <=  250 && stops >= 30) {
+	var moreToBeDone = true;
 	console.log("We get to explore even more then we thought! Yay!");
 	} else {
 		moreToBeDone = false;
 		console.log("We are broke so no more spending money for us.");
 		};
-		return moreToBeDone;
 };
 
 // Number Function
-var exploringTime = function (hrOfTraveling) {
+var exploringTime = function (totalFlightHours) {
 	letsFlyAway = "Thai Air"
-	while (hrOfTraveling >= 40) {
-	console.log ("Just want to start vacation yet there is still " + hrOfTraveling + " to go!  Teleporting needs to happen now. Want nothing more then to see" + locations + "ASAP!");
-	hrOfTravleing--;
+	var hoursLeftToTravel = 40;
+	if (totalFlightHours > hoursLeftToTravel) {
+		var remainingHours = (totalFlightHours - hoursLeftToTravel);
+		console.log("we are here");
+		while (remainingHours > 0){
+			console.log ("Just want to start vacation yet there is still " + remainingHours + " to go!  Teleporting needs to happen now. Want nothing more then to see " + locations[remainingHours] + " ASAP!");
+			remainingHours--;
+		}
 	};
-	return hrOfTraveling;	
 };
 
-// String Function
-var jetLag (country, hrOfTraveling) {
-	mustSleep = "heads hits the pillow";
-	hotelRoom = "W Hotel"
-	console.log ("Now that we have arrived in" + country + " we will let our" + mustSleep + "in the amazing" + hotelRoom + "so that we can actually explore" + locations + "with as little jet lag as possible");
-	return hotelRoom
-};
 
-// Array Function
-var 
